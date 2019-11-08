bde1f7154e5e6f8b2921de8a201de1fb38b4501c
atlasax <bp@lu-e.de>
2019-11-08 14:15:38 +0100

    mockup initial commit

---
+++ diabeatit/src/main/java/de/tu_darmstadt/informatik/tk/diabeatit/HomeActivity.java
@@ -0,0 +1,106 @@
+package de.tu_darmstadt.informatik.tk.diabeatit;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Settings
+
+        // TODO
+
+        // Assistant
+
+        View nestedScrollView = (View) findViewById(R.id.assistant_scrollview);
+        BottomSheetBehavior assistant = BottomSheetBehavior.from(nestedScrollView);
+        final TextView peek = (TextView) findViewById(R.id.assistant_peek);
+
+        assistant.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(View bottomSheet, int newState) {
+
+                if (newState == BottomSheetBehavior.STATE_COLLAPSED)
+                    peek.setCompoundDrawablesWithIntrinsicBounds(peek.getContext().getDrawable(R.drawable.ic_slideup), null, null, null);
+
+                if (newState == BottomSheetBehavior.STATE_EXPANDED)
+                    peek.setCompoundDrawablesWithIntrinsicBounds(peek.getContext().getDrawable(R.drawable.ic_slidedown), null, null, null);
+
+            }
+
+            @Override
+            public void onSlide(View view, float v) {}
+        });
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_settings, R.id.nav_device_sensor, R.id.nav_device_pump, R.id.nav_device_tracker)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        ((NavigationView) findViewById(R.id.nav_view)).setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                if (menuItem.getItemId() == R.id.nav_settings)
+                    startActivity(new Intent(HomeActivity.this, SettingsActivity.class));
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
