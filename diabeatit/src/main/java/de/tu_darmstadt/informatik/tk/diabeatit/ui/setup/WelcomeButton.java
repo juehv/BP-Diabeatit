23e312163771211e7d700baf1f1b9816b1648dc3
atlasax <bp@lu-e.de>
2019-11-10 19:59:49 +0100

    updated setup

--- diabeatit/src/main/java/de/tu_darmstadt/informatik/tk/diabeatit/ui/setup/WelcomeButton.java
+++ diabeatit/src/main/java/de/tu_darmstadt/informatik/tk/diabeatit/ui/setup/WelcomeButton.java
@@ -71,7 +71,7 @@ public class WelcomeButton extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome_butotn, container, false);
+        return inflater.inflate(R.layout.fragment_welcome_button, container, false);
     }
 
     @Override
