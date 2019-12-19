52abb774d2c204f194a8a9627b1acd1c138c6921
Dominik Gojny <dominik@dr-gojny.de>
2019-12-19 21:20:39 +0100

    Commit-Setup

--- diabeatit/src/main/java/de/tu_darmstadt/informatik/tk/diabeatit/ui/setup/SetupInsight.java
+++ diabeatit/src/main/java/de/tu_darmstadt/informatik/tk/diabeatit/ui/setup/SetupInsight.java
@@ -1,32 +1,30 @@
 package de.tu_darmstadt.informatik.tk.diabeatit.ui.setup;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import de.tu_darmstadt.informatik.tk.diabeatit.R;
 
 public class SetupInsight extends AppCompatActivity {
 
+    protected Button backToSetup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_insight);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        backToSetup = (Button) findViewById(R.id.return_to_setup);
+        backToSetup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                finish();
+                startActivity(new Intent(SetupInsight.this, SetupActivity.class));
             }
         });
     }
