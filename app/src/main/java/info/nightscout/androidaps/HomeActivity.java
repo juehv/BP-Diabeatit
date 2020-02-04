cf78ae5d05f05b6cec0af38e74b72d419d089c96
atlasax <bp@lu-e.de>
2020-02-04 17:11:39 +0100

    fixed file names 2

--- app/src/main/java/info/nightscout/androidaps/HomeActivity.java
+++ app/src/main/java/info/nightscout/androidaps/HomeActivity.java
@@ -215,7 +215,7 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
 
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
+        getMenuInflater().inflate(R.menu.d_home, menu);
         return true;
     }
 
