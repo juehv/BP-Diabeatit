d0ea9fca75965c942a159d96c013fcf560996e22
skeleten <me@skeleten.me>
2020-02-06 23:51:47 +0100

    Tweak tests

--- app/src/androidTest/java/info/nightscout/androidaps/KiPredictionsInstrumentedTest.java
+++ app/src/androidTest/java/info/nightscout/androidaps/KiPredictionsInstrumentedTest.java
@@ -1,6 +1,7 @@
 package info.nightscout.androidaps;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -10,7 +11,6 @@ import org.junit.runner.RunWith;
 
 import info.nightscout.androidaps.diabeatit.predictions.SimpleBgReadingsInput;
 import info.nightscout.androidaps.diabeatit.predictions.TensorflowPredictionModel;
-
 @RunWith(AndroidJUnit4.class)
 public class KiPredictionsInstrumentedTest {
     @Test
@@ -24,9 +24,15 @@ public class KiPredictionsInstrumentedTest {
         SimpleBgReadingsInput inputs = new SimpleBgReadingsInput(inputValues);
 
         float[] predictions = model.predict(inputs);
-        System.err.printf("%s: Predictions\n", KiPredictionsInstrumentedTest.class.getSimpleName());
-        for (float prediction : predictions) {
-            System.err.printf("%3f  ", prediction);
+        Log.w("TEST", String.format("%s: Predictions\n", KiPredictionsInstrumentedTest.class.getSimpleName()));
+        StringBuilder buf = new StringBuilder();
+        for (int i = 0; i < predictions.length; i++) {
+            if (i % 3 == 0) {
+                Log.w("TEST", buf.toString());
+                buf = new StringBuilder();
+            }
+            float prediction = predictions[i];
+            buf.append(String.format("%3f\t", prediction));
         }
         System.err.println();
     }
