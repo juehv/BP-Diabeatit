386b283180a64566ee1dcb72f7c3fcfe7589ab24
atlasax <bp@lu-e.de>
2020-02-29 11:48:09 +0100

    fix alerts

--- app/src/main/java/info/nightscout/androidaps/diabeatit/ui/AlertHistoryActivity.java
+++ app/src/main/java/info/nightscout/androidaps/diabeatit/ui/AlertHistoryActivity.java
@@ -1,11 +1,15 @@
 package info.nightscout.androidaps.diabeatit.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import info.nightscout.androidaps.R;
+import info.nightscout.androidaps.diabeatit.assistant.alert.Alert;
 import info.nightscout.androidaps.diabeatit.assistant.alert.AlertStore;
+import info.nightscout.androidaps.diabeatit.assistant.alert.AlertStoreListener;
 import info.nightscout.androidaps.diabeatit.assistant.alert.DismissedAlertsManager;
 
 public class AlertHistoryActivity extends AppCompatActivity {
@@ -19,6 +23,42 @@ public class AlertHistoryActivity extends AppCompatActivity {
 		setTheme(R.style.diabeatit);
 
 		AlertStore.dismissedAlerts = new DismissedAlertsManager(getApplicationContext(), findViewById(R.id.alert_history_layout));
+
+	  TextView dismissedEmptyT = findViewById(R.id.alert_history_empty_notice);
+
+	  AlertStore.attachListener(new AlertStoreListener() {
+
+		  @Override
+		  public void onNewAlert(Alert alert) {
+		  }
+
+		  @Override
+		  public void onAlertDismissed(Alert alert) {
+
+			  dismissedEmptyT.setVisibility(View.GONE);
+
+		  }
+
+		  @Override
+		  public void onAlertRestored(Alert alert) {
+
+			  if (AlertStore.getDismissedAlerts().length == 0)
+				  dismissedEmptyT.setVisibility(View.VISIBLE);
+
+		  }
+
+		  @Override
+		  public void onAlertsCleared() {
+		  }
+
+		  @Override
+		  public void onDataSetInit() {
+
+			  dismissedEmptyT.setVisibility(AlertStore.getDismissedAlerts().length == 0 ? View.VISIBLE : View.GONE);
+
+		  }
+
+	  });
 
   }
 
