29a763465b986f367a9415bbabd1077d42dfb6b8
skeleten <me@skeleten.me>
2020-03-15 13:46:37 +0100

    WIP

---
+++ app/src/main/java/info/nightscout/androidaps/diabeatit/ui/DebugStylesActivity.java
@@ -0,0 +1,16 @@
+package info.nightscout.androidaps.diabeatit.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import info.nightscout.androidaps.R;
+
+public class DebugStylesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug_styles);
+    }
+}
--- app/src/main/java/info/nightscout/androidaps/diabeatit/ui/ManualInsulinEntryActivity.java
+++ app/src/main/java/info/nightscout/androidaps/diabeatit/ui/ManualInsulinEntryActivity.java
@@ -54,6 +54,7 @@ public class ManualInsulinEntryActivity extends AppCompatActivity {
 
     private void setDateTimeButtonClick() {
         DatePickerDialog diag = new DatePickerDialog(this,
+                R.style.diabeatit_PopupWindow,
                 ((v, y, m, d) -> {
                     setDate(y, m, d);
                     selectTime();
@@ -89,11 +90,13 @@ public class ManualInsulinEntryActivity extends AppCompatActivity {
     }
 
     private void selectTime() {
-          TimePickerDialog diag = new TimePickerDialog(this,
-                (v, h, m) -> setTime(h, m),
-                timestamp.get(Calendar.HOUR_OF_DAY),
-                timestamp.get(Calendar.MINUTE),
-                  true);
+        TimePickerDialog diag = new TimePickerDialog(this,
+            R.style.diabeatit_TimePicker,
+            (v, h, m) -> setTime(h, m),
+            timestamp.get(Calendar.HOUR_OF_DAY),
+            timestamp.get(Calendar.MINUTE),
+              true);
+
         diag.show();
     }
 
