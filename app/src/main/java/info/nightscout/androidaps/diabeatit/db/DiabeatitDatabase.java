cea90e9a016e9fbff84d6d1ad687a0c8c0dbd0c9
skeleten <me@skeleten.me>
2020-03-29 16:57:55 +0200

    add docs

--- app/src/main/java/info/nightscout/androidaps/diabeatit/db/DiabeatitDatabase.java
+++ app/src/main/java/info/nightscout/androidaps/diabeatit/db/DiabeatitDatabase.java
@@ -10,14 +10,26 @@ import info.nightscout.androidaps.diabeatit.log.event.CarbsEvent;
 import info.nightscout.androidaps.diabeatit.log.event.NoteEvent;
 import info.nightscout.androidaps.diabeatit.log.event.SportsEvent;
 
-@Database(entities = {Alert.class, BolusEvent.class, CarbsEvent.class, SportsEvent.class, NoteEvent.class}, version = 1, exportSchema = false)
+/** Application database for the objects managed by the DiaBEATit project. */
+@Database(entities = {
+            Alert.class,
+            BolusEvent.class,
+            CarbsEvent.class,
+            SportsEvent.class,
+            NoteEvent.class
+        }, version = 1, exportSchema = false)
 @TypeConverters({info.nightscout.androidaps.diabeatit.db.TypeConverters.class})
 public abstract class DiabeatitDatabase extends RoomDatabase {
 
+    /** Get a data access object for {@link Alert}s */
     public abstract AlertDao alertDao();
+    /** Get a data access object for {@link BolusEvent} */
     public abstract BolusEventDao bolusEventDao();
+    /** Get a data access object for {@link CarbsEvent} */
     public abstract CarbsEventDao carbsEventDao();
+    /** Get a data access object for {@link SportsEvent} */
     public abstract SportsEventDao sportsEventDao();
+    /** Get a data access object for {@link NoteEvent} */
     public abstract NoteEventDao noteEventDao();
 
 }
